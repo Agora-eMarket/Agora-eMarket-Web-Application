f969abd89edb0ea6a3eff6fc6b5705766199d1e5
alrieq <alrieq@live.com>
2023-03-30 01:27:15 +0300

    fix footer

--- 405-react-project/src/App.js
+++ 405-react-project/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Footer.jsx";
 import Showcase from "./components/Showcase.jsx";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <Showcase />
+      <Footer />
     </>
   );
 }
