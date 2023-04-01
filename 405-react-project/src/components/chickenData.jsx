90afbd5bfeee80c39af979b224fddce68b87174b
alrieq <alrieq@live.com>
2023-04-01 23:37:28 +0300

    add menu items

---
+++ 405-react-project/src/components/chickenData.jsx
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        img: "./images/Chicken-Fillet-Nuggets-Meal.png",
+        name: "Chicken-Fillet-Nuggets",
+        discreption: "7 or 10 pieces chicken fillet nuggets come with...",
+        price: 14
+    }, {
+
+        id: 2,
+        img: "./images/Albaik-chicken-thumb-1.png",
+        name: "Albaik-chicken-Meal",
+        discreption: "4 or 8 pieces chicken meals come with fries, a...",
+        price: 8
+    }
+    , {
+
+        id: 3,
+        img: "./images/Chicken-fillet-burger-thumnb.png",
+        name: "Chicken-fillet-burger",
+        discreption: "7 or 10 pieces chicken fillet nuggets come with...",
+        price: 5
+    },
+    {
+        id: 4,
+        img: "./images/chicken-fillet-nuggets-snack.png",
+        name: "chicken-fillet-nuggets-snack",
+        discreption: "5 chicken fillet bite-sized nuggets come with...",
+        price: 3
+    }
+
+]
---
+++ 405-react-project/src/components/seafoodData.jsx
@@ -0,0 +1,9 @@
+export default [
+    {
+        id: 1,
+        img: "./images/Chicken-Fillet-Nuggets-Meal.png",
+        name: "jumbo-shrimp-meal",
+        discreption: "8 or 10 jumbo shrimp pieces come with fries, a bun and your choice of 2 sauces (cocktail/nugget/garlic sauce)",
+        price: 23
+    }
+]
