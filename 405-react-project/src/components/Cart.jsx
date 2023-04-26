e04bea2f7a22fa90ba6abf4b61e4256401267d0f
AMM48 <abdulrahmanalmehdar48@gmail.com>
2023-04-26 12:01:45 +0300

    added local storage for cart

--- 405-react-project/src/components/Cart.jsx
+++ 405-react-project/src/components/Cart.jsx
@@ -1,72 +1,45 @@
 import React from "react";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CartLayout from "./CartLayout";
 
 export default function Cart() {
-    //replace from here until you see ---------- with cart from local storage
-    const [dairyEggsData, setDairyEggsData] = React.useState([]);
-    const [fruitsVegetablesData, setFruitsVegetablesData] = React.useState([]);
+  const cartItems = JSON.parse(localStorage.getItem("items")) || [];
 
-    React.useEffect(() => {
-        const fetchMenu = async () => {
-            var requestOptions = {
-                method: "GET",
-                redirect: "follow",
-            };
-            try {
-                const response = await fetch(
-                    "http://localhost/menu.php?category=all",
-                    requestOptions
-                );
-                const result = await response.json();
-                const dairyEggs = result.filter(
-                    (item) => item.category === "Dairy" || item.category === "Eggs"
-                );
-                setDairyEggsData(dairyEggs);
-                const fruitsVegetables = result.filter(
-                    (item) => item.category === "Fruits" || item.category === "Vegetables"
-                );
-                setFruitsVegetablesData(fruitsVegetables);
-            } catch (e) {
-                console.error(e.message);
-            }
-        };
-        fetchMenu();
-    }, []);
+  // Calculate the cost of the items in the cart
+  let price = 0;
+  cartItems.forEach((item) => {
+    price += parseFloat(item.price);
+  });
 
-    const dairyEggs = dairyEggsData.map((item) => {
-        return <CartLayout key={item.id} item={item} />;
-    });
-    const fruitsVegetables = fruitsVegetablesData.map((item) => {
-        return <CartLayout key={item.id} item={item} />;
-    });
-    //----------------------------------- stop here --------------------------------------
-    var price=0;
-    const cost = dairyEggsData.map((item) => {
-        return(price+= parseFloat( item.price))
-    })
-    const vat = (price*0.15).toFixed(2);
-    const totaPrice = parseFloat(price) + parseFloat(vat);
-    return (
-        <>
+  // Calculate the VAT and total price
+  const vat = (price * 0.15).toFixed(2);
+  const totalPrice = parseFloat(price) + parseFloat(vat);
 
-            <Navbar />
-            <div className="cart-container">
-                <div className="cart-inside">
-                    {dairyEggs}
-                    <hr />
-                    <div className="cart-stats">
-                        <h3>Cost: {price}</h3>
-                        <h3>Vat 15%: {vat}</h3>
-                        <h3>Total Cost: {totaPrice}</h3>
-                        <button className="btn">Proceed to checkout</button>
-                    </div>
-                </div>
-
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="cart-container">
+        <div className="cart-inside">
+          {cartItems.map((item, index) => (
+            <CartLayout
+              key={index}
+              img={item.img}
+              name={item.name}
+              price={item.price}
+              quantity={item.quantity}
+            />
+          ))}
+          <hr />
+          <div className="cart-stats">
+            <h3>Cost: {price.toFixed(2)}</h3>
+            <h3>Vat 15%: {vat}</h3>
+            <h3>Total Cost: {totalPrice.toFixed(2)}</h3>
+            <button className="checkout-btn">Proceed to checkout</button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
-
--- 405-react-project/src/components/Contact.jsx
+++ 405-react-project/src/components/Contact.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import dollarSign from "./dollar-sign.png";
 export default function Contact({ props }) {
+  const [quantity, setQuantity] = React.useState(1);
+
+  function handleQuantityChange(event) {
+    const newQuantity = Number(event.target.value);
+    if (newQuantity < 1) {
+      setQuantity(1);
+    } else {
+      setQuantity(newQuantity);
+    }
+  }
+
   const addCart = () => {
-    console.log(props.name, " has been added to cart");
+    const newItem = {
+      img: props.img,
+      name: props.name,
+      price: props.price * quantity,
+      quantity: quantity,
+    };
+
+    // Retrieve existing items from local storage
+    const storedItems = localStorage.getItem("items");
+
+    let parsedItems = [];
+
+    if (storedItems && storedItems !== "") {
+      // Parse the stored items string
+      parsedItems = JSON.parse(storedItems);
+    }
+
+    // Check if the new item already exists in the cart
+    const existingItem = parsedItems.find((item) => item.name === newItem.name);
+
+    if (existingItem) {
+      // If the item exists, update its quantity
+      existingItem.quantity += newItem.quantity;
+      existingItem.price += newItem.price;
+    } else {
+      // If the item does not exist, add it to the cart
+      parsedItems.push(newItem);
+    }
+
+    // Store the updated cart items in local storage
+    localStorage.setItem("items", JSON.stringify(parsedItems));
   };
   return (
     <div className="contact-card">
@@ -12,17 +53,19 @@ export default function Contact({ props }) {
         <p>{props.discreption}</p>
       </div>
       <div className="info-group">
-        <img src={dollarSign} alt="" />
-        <p>{props.price} SAR </p>
+        <p id="priceTag">SAR {props.price}</p>
       </div>
       <div className="flex">
-        <button
-          className="add-btn"
-          onClick={addCart}
-        >
+        <button className="add-btn" onClick={addCart}>
           Add to cart
         </button>
-        <input type="number" className="quantity" min="1"></input>
+        <input
+          type="number"
+          className="quantity"
+          min="1"
+          value={quantity}
+          onChange={handleQuantityChange}
+        ></input>
       </div>
     </div>
   );
